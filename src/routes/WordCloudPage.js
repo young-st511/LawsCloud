78516e53a6843f4ee38c2334e0a2b6c59943067e
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2022-10-12 19:11:32 +0900

    Add: WordCloudPage 컴포넌트 추가 ##기능 국회 발의안 워드클라우드를 연도별, 기수별로 보여주는 페이지 ##설명 연도와 기수를 선택하여 워드클라우드를 볼 수 있습니다. 사이즈를 attribute로 지정할 수 있습니다. 비교하기 페이지로 갈 수 있는 버튼이 있습니다.

---
+++ src/routes/WordCloudPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Title from "../components/wordcloud-components/Title";
+import SearchBar from "../components/wordcloud-components/SearchBar(not-use)";
+import WordCloudSection from "../components/wordcloud-components/WordCloudSection";
+import styled from "styled-components";
+
+const WordCloudPage = ({setSearch}) => {
+  return (
+    <StyledWrapper>
+      <Title />
+      <SearchBar setSearch={setSearch} />
+      <WordCloudSection setSearch={setSearch} size={100} />
+      <button type="button" className="compare-button">
+        비교해보기
+      </button>
+    </StyledWrapper>
+  );
+};
+
+export default WordCloudPage;
+
+WordCloudPage.propTypes = {
+  setSearch: PropTypes.func.isRequired,
+};
+
+const StyledWrapper = styled.div`
+  width: 1200px;
+  height: 1400px;
+  margin: 0 auto;
+
+  .compare-button {
+    margin: 10px auto;
+    display: flex;
+    width: 110px;
+    height: 36px;
+
+    background: #000000;
+    border-radius: 15px;
+    border-style: none;
+
+    font-weight: 600;
+    font-size: 16px;
+    color: #ffffff;
+
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    :hover {
+      background-color: #223131;
+      transition: background-color 0.24s;
+    }
+  }
+`;
