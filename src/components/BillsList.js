55354ba1a9c5e63f18d155afadf05c6c04fa4990
Lee-SeungHoon <vheh0901@gmail.com>
2022-10-16 19:33:47 +0900

    Fix: 조회수 기능 데이터 베이스 위치 변경

--- src/components/BillsList.js
+++ src/components/BillsList.js
@@ -3,11 +3,29 @@ import {StyledBillTable, StyledBillThead, StyledBillTbody} from "../style/Styled
 import ExcelFilterButton from "./ExcelFilterButton";
 import BillsModal from "./Modal/BillsModal";
 import TotalViews from "./TotalViews/TotalViews";
+import {set, ref, onValue} from "firebase/database";
+import {firebasedatabase} from "./Firebase/firebase";
 const headerMeta = ["의안명", "제안자", "상임위원회", "조회수", "추천수"];
 
 const BillsList = ({billList, setExcelFilter, setPage}) => {
   const [onModal, setOnModal] = useState(false);
   const [billsInformation, setBillsInformation] = useState({});
+  const [viewCount, setViewCount] = useState(0);
+
+  const setView = (data) => {
+    const firebaseRef = ref(firebasedatabase, "billId/" + data.BILL_ID);
+
+    onValue(firebaseRef, (snapshot) => {
+      setViewCount(snapshot.val().count);
+    });
+
+    set(firebaseRef, {
+      name: data.BILL_NAME,
+      count: viewCount + 1,
+    });
+
+    setViewCount(0);
+  };
 
   return (
     <>
@@ -29,6 +47,7 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
                   onClick={() => {
                     setOnModal(!onModal);
                     setBillsInformation(data);
+                    setView(data);
                   }}>
                   {data.BILL_NAME}
                 </td>
@@ -38,7 +57,7 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
                 </td>
                 <td>({data.COMMITTEE})</td>
                 <td>
-                  <TotalViews billId={data.BILL_ID} billsListPage={true} />
+                  <TotalViews billId={data.BILL_ID} />
                 </td>
                 <td>0</td>
               </tr>
--- src/components/TotalViews/TotalViews.js
+++ src/components/TotalViews/TotalViews.js
@@ -1,46 +1,27 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import ViewArea from "../../style/StyledModal";
 
 // getFirestore
-import {dbService} from "../Firebase/firebase";
-import {getDoc, doc, setDoc} from "@firebase/firestore";
+import {firebasedatabase} from "../Firebase/firebase";
+import {ref, get, child} from "firebase/database";
 
-function TotalViews({billId, billsListPage}) {
+function TotalViews({billId}) {
   const [viewCount, setViewCount] = useState(0);
 
-  useEffect(() => {
-    const getcount = async (docRef) => {
-      const docSnap = await getDoc(docRef);
-      if (docSnap.data() === undefined && billsListPage === false) {
-        setDoc(doc(dbService, `${billId}`, "viewCount"), {
-          count: 1,
-        });
-      }
-      if (docSnap.data() && billsListPage === false) {
-        setDoc(doc(dbService, `${billId}`, "viewCount"), {
-          count: docSnap.data().count + 1,
-        });
-        setViewCount(docSnap.data().count);
-        console.log(docSnap.data());
-      }
-      if (docSnap.data() === undefined && billsListPage === true) {
-        setViewCount(0);
-      }
-      if (docSnap.data() && billsListPage === true) {
-        setViewCount(docSnap.data().count);
-      }
-    };
-    const docRef = doc(dbService, `${billId}`, "viewCount");
-    getcount(docRef);
-  }, [billId]);
+  get(child(ref(firebasedatabase), `billId/${billId}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      setViewCount(snapshot.val().count);
+    } else {
+      console.log("No data available");
+    }
+  });
 
   return <ViewArea>{viewCount}</ViewArea>;
 }
 
 TotalViews.propType = {
   billId: PropTypes.string.isRequired,
-  billsListPage: PropTypes.bool.isRequired,
 };
 
 export default TotalViews;
