8ae6fa8d6d0b2d0211217994d465c49a82f80e3e
DESKTOP-JTDD7A9\vheh0 <vheh0901@gmail.com>
2022-10-18 22:42:08 +0900

    Fix: 좋아요 상태 ip에 저장

--- src/components/BillList/BillsList.js
+++ src/components/BillList/BillsList.js
@@ -6,16 +6,20 @@ import TotalViews from "../TotalViews/TotalViews";
 import LikeNum from "../likeButton/LikeNum";
 import {set, ref, get, child, update} from "firebase/database";
 import {firebasedatabase} from "../Firebase/firebase";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {userIp, userLikeState} from "../../recoil/store";
+
 const headerMeta = ["의안명", ["제안자", "(제안 일자)"], ["상임위원회", "(소관부처)"], "조회수", "추천수"];
 
 const BillsList = ({billList, setExcelFilter, setPage}) => {
   const [onModal, setOnModal] = useState(false);
   const [billsInformation, setBillsInformation] = useState({});
   const [viewCount, setViewCount] = useState(0);
+  const [likeState, setLikeState] = useRecoilState(userLikeState);
+  const ip = useRecoilValue(userIp);
 
   const setView = (data) => {
     const firebaseRef = ref(firebasedatabase, "billId/" + data.BILL_ID);
-
     get(child(ref(firebasedatabase), "billId/" + data.BILL_ID)).then((snapshot) => {
       if (snapshot.exists()) {
         update(ref(firebasedatabase, `billId/${data.BILL_ID}`), {
@@ -33,21 +37,26 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
     setViewCount(0);
   };
 
+  const getIpLikeInfo = (data) => {
+    const firebaseRef = ref(firebasedatabase);
+    const userIp = ip.split(".").join("");
+    get(child(firebaseRef, "billId/" + data.BILL_ID)).then((snapshot) => {
+      if (snapshot.val().likes) {
+        const data = snapshot.val().likes[userIp];
+        setLikeState(data);
+      } else {
+        setLikeState(false);
+      }
+    });
+  };
+
   return (
     <>
       <StyledBillTable>
         <StyledBillThead>
           {headerMeta.map((data, idx) => (
             <div key={idx} className="header">
-              {idx === 1 || idx === 2
-                ? [
-                    data[0],
-                    <span key={idx} className="subHeader">
-                      {data[1]}
-                    </span>,
-                  ]
-                : data}
-              {idx === 2 && <ExcelFilterButton setExcelFilter={setExcelFilter} setPage={setPage} />}
+              {data} {idx === 2 && <ExcelFilterButton setExcelFilter={setExcelFilter} setPage={setPage} />}
             </div>
           ))}
         </StyledBillThead>
@@ -61,6 +70,7 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
                     setOnModal(!onModal);
                     setBillsInformation(data);
                     setView(data);
+                    getIpLikeInfo(data);
                   }}>
                   {data.BILL_NAME}
                 </div>
@@ -70,16 +80,14 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
                   {data.PROPOSE_DT}
                 </div>
                 <div className="item">({data.COMMITTEE ? data.COMMITTEE : "소속 없음"})</div>
-                <div className="item">
-                  <TotalViews billId={data.BILL_ID} />
-                </div>
-                <div className="item">
-                  <LikeNum billId={data.BILL_ID} />
-                </div>
+                <TotalViews billId={data.BILL_ID} />
+                <LikeNum billId={data.BILL_ID} />
               </div>
             ))
           ) : (
-            <div>loading...</div>
+            <tr>
+              <td>loding...</td>
+            </tr>
           )}
         </StyledBillTbody>
       </StyledBillTable>
