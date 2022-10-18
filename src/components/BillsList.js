51a439aaa28f9c247186f710fce66ddc2ef400d7
DESKTOP-JTDD7A9\vheh0 <vheh0901@gmail.com>
2022-10-18 22:21:05 +0900

    Fix: 좋아요 클릭 상태 ip에 저장

--- src/components/BillsList.js
+++ src/components/BillsList.js
@@ -3,18 +3,22 @@ import {StyledBillTable, StyledBillThead, StyledBillTbody} from "../style/Styled
 import ExcelFilterButton from "./ExcelFilterButton";
 import BillsModal from "./Modal/BillsModal";
 import TotalViews from "./TotalViews/TotalViews";
-import {set, ref, get, child, update} from "firebase/database";
+import {set, ref, get, child, update, onValue} from "firebase/database";
 import {firebasedatabase} from "./Firebase/firebase";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {userIp, userLikeState} from "../recoil/store";
+import LikeNum from "./likeButton/LikeNum";
 const headerMeta = ["의안명", "제안자", "상임위원회", "조회수", "추천수"];
 
 const BillsList = ({billList, setExcelFilter, setPage}) => {
   const [onModal, setOnModal] = useState(false);
   const [billsInformation, setBillsInformation] = useState({});
   const [viewCount, setViewCount] = useState(0);
+  const [likeState, setLikeState] = useRecoilState(userLikeState);
+  const ip = useRecoilValue(userIp);
 
   const setView = (data) => {
     const firebaseRef = ref(firebasedatabase, "billId/" + data.BILL_ID);
-
     get(child(ref(firebasedatabase), "billId/" + data.BILL_ID)).then((snapshot) => {
       if (snapshot.exists()) {
         update(ref(firebasedatabase, `billId/${data.BILL_ID}`), {
@@ -30,6 +34,19 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
     });
 
     setViewCount(0);
+  };
+
+  const getIpLikeInfo = (data) => {
+    const firebaseRef = ref(firebasedatabase);
+    const userIp = ip.split(".").join("");
+    get(child(firebaseRef, "billId/" + data.BILL_ID)).then((snapshot) => {
+      if (snapshot.val().likes) {
+        const data = snapshot.val().likes[userIp];
+        setLikeState(data);
+      } else {
+        setLikeState(false);
+      }
+    });
   };
 
   return (
@@ -52,6 +69,7 @@ const BillsList = ({billList, setExcelFilter, setPage}) => {
                     setOnModal(!onModal);
                     setBillsInformation(data);
                     setView(data);
+                    getIpLikeInfo(data);
                   }}>
                   {data.BILL_NAME}
                 </div>
