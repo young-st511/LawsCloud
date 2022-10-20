d629e75d60562015792ed47fcc0fda3ab97b2749
kykson2 <kykson2@naver.com>
2022-10-20 17:58:38 +0900

    Mod: 최신댓글 최신순으로 보여지게 정렬

--- src/components/Reply/RecentReply.js
+++ src/components/Reply/RecentReply.js
@@ -2,38 +2,38 @@ import React, {useState, useEffect} from "react";
 
 import RecentReplysView from "./RecentReplyView";
 
-import {ref, get, child} from "firebase/database";
+import {ref, get, child, onValue} from "firebase/database";
 import {firebasedatabase, dbService} from "../Firebase/firebase";
 import {onSnapshot, collection} from "firebase/firestore";
 
 export default function RecentReplys() {
   const [recentReply, setRecentReply] = useState([]);
-  useEffect(() => {
-    get(child(ref(firebasedatabase), `billId/`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        const viewed = snapshot.val();
-        Object.entries(viewed).map((bill) => {
-          onSnapshot(collection(dbService, `${bill[0]}`), (sanpshot) => {
-            const replyArray = sanpshot.docs.map((doc) => ({
-              id: doc.id,
-              text: doc.text,
-              age: doc.age,
-              ...doc.data(),
-            }));
 
-            replyArray.map((item) => {
-              if (item.text && item.age) {
-                item["billName"] = bill[1].name;
-                item["billId"] = bill[0];
+  get(child(ref(firebasedatabase), `billId/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      const viewed = snapshot.val();
+      Object.entries(viewed).map((bill) => {
+        onSnapshot(collection(dbService, `${bill[0]}`), (sanpshot) => {
+          const replyArray = sanpshot.docs.map((doc) => ({
+            id: doc.id,
+            text: doc.text,
+            age: doc.age,
+            ...doc.data(),
+          }));
 
-                setRecentReply((recentReply) => [...recentReply, item]);
-              }
-            });
+          replyArray.map((item) => {
+            if (item.text && item.age) {
+              item["billName"] = bill[1].name;
+              item["billId"] = bill[0];
+              item["seconds"] = item.createdAt.seconds;
+
+              setRecentReply((recentReply) => [...recentReply, item]);
+            }
           });
         });
-      }
-    });
-  }, []);
+      });
+    }
+  });
 
   return (
     <>
