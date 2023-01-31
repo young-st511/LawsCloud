72c310abb1f6e8f9b02dfd9448ed6c131cd1418c
young-st511 <asdk511@gmail.com>
2023-02-01 01:40:02 +0900

    🐛 Fix: Fix Firebase Environment Value Error

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -4,14 +4,16 @@ import {getFirestore} from "firebase/firestore";
 import {getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
-  authDomain: process.env.REACT_AUTH_DOMAIN,
-  projectId: process.env.REACT_PROJECT_ID,
-  storageBucket: process.env.REACT_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID,
-  databaseURL: process.env.REACT_DATABASE_URL,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
+
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 
